7794a8aa3dfd670a9d87d65bdfe2bb46b7fbaf3c
jc sarmiento <jacinto011200@gmail.com>
2024-10-17 00:01:04 +0800

    add rhe input data

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,3 +1,10 @@
 @props(['disabled' => false])
 
-<input @disabled($disabled) {{ $attributes->merge(['class' => 'border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm']) }}>
+<input 
+    @disabled($disabled) 
+    {{ $attributes->merge([
+        'class' => 'border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm 
+                    w-full p-2 md:p-3 lg:p-4 
+                    text-sm md:text-base lg:text-lg'
+    ]) }} 
+/>
