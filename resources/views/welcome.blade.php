404fe020f5ad4999dc443227a76954ce2ecae3a2
jc sarmiento <jacinto011200@gmail.com>
2024-10-17 01:13:24 +0800

    add the data

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -138,7 +138,7 @@
                 </div>
             </div>
         </div>
-
+        <!-- featured product -->
     </div>
 </section>
 {{-- End of Featured Products --}}
@@ -266,87 +266,7 @@
                     <h5 class="theme-color">Our Collection</h5>
                 </div>
             </div>
-
-            <div class="our-product products-c">
-                @foreach ($best_seller as $best)
-                <div>
-                    <div class="product-box">
-                        <div class="img-wrapper">
-                            <a href="{{route('product-details', $best->id)}}">
-                                <img src="{{ asset('images/' . $best->main_image) }}"
-                                    class="w-100 bg-img blur-up lazyload" alt="">
-                            </a>
-                            <div class="circle-shape"></div>
-                            <span class="background-text">Hand</span>
-                            <div class="cart-wrap">
-                                <ul>
-                                    <li>
-                                        <a href="{{route('cart')}}">
-                                            <i data-feather="shopping-cart"></i>
-                                        </a>
-                                    </li>
-                                    <li><a href="{{route('product-details', $best->id)}}"><i data-feather="eye"></i></a>
-                                    </li>
-                                    <li>
-                                        <a href="" class="wishlist">
-                                            <i data-feather="heart"></i>
-                                        </a>
-                                    </li>
-                                    {{-- <li>
-                                        <a href="{{route('cart')}}" class="addtocart-btn" data-bs-toggle="modal"
-                                            data-bs-target="#addtocart">
-                                            <i data-feather="shopping-cart"></i>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="{{route('product-details', $item->id)}}" data-bs-toggle="modal"
-                                            data-bs-target="#quick-view">
-                                            <i data-feather="eye"></i>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="" class="wishlist">
-                                            <i data-feather="heart"></i>
-                                        </a>
-                                    </li> --}}
-                                </ul>
-                            </div>
-                        </div>
-                        <div class="product-style-3 product-style-chair">
-                            <div class="product-title d-block mb-0">
-                                <div class="r-price">
-                                    <div class="theme-color">&#8369;{{$best->price}}</div>
-                                    <div class="main-price">
-                                        <ul class="rating mb-1 mt-0">
-                                            <li>
-                                                <i class="fas fa-star theme-color"></i>
-                                            </li>
-                                            <li>
-                                                <i class="fas fa-star theme-color"></i>
-                                            </li>
-                                            <li>
-                                                <i class="fas fa-star"></i>
-                                            </li>
-                                            <li>
-                                                <i class="fas fa-star"></i>
-                                            </li>
-                                            <li>
-                                                <i class="fas fa-star"></i>
-                                            </li>
-                                        </ul>
-                                    </div>
-                                </div>
-                                <p class="font-light mb-sm-2 mb-0">{{$best->category}}</p>
-                                <a href="product/details.html" class="font-default">
-                                    <h5>{{$best->title}}</h5>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                @endforeach
-
-            </div>
+            <!-- featured product best seler -->
         </div>
     </div>
 </section>
