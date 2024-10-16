2cd7fef4f23ee9f1fd27c5b6b6c272bd8aa057c2
jc sarmiento <jacinto011200@gmail.com>
2024-10-17 00:46:34 +0800

    add the data of the logo

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,5 +1,5 @@
 <div class="flex items-center justify-center">
-    <div class="flex items-center justify-center h-24 w-full">
-        <img src="{{ asset('admin_assets/img/logo/gawang-gamat.png') }}" alt="Gawang Gamat" class="h-full w-full object-contain">
+    <div class="flex items-center justify-center">
+        <img src="{{ asset('admin_assets/img/logo/gawang-gamat.png') }}" alt="Gawang Gamat" class="object-contain">
     </div>
 </div>
