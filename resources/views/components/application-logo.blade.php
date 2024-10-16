055c8fb2d3a34daeb925a75455937c832ae32f20
jc sarmiento <jacinto011200@gmail.com>
2024-10-17 00:47:35 +0800

    add the max size

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,5 +1,5 @@
-<div class="flex items-center justify-center">
+<div class="flex items-center justify-center h-16">
     <div class="flex items-center justify-center">
-        <img src="{{ asset('admin_assets/img/logo/gawang-gamat.png') }}" alt="Gawang Gamat" class="object-contain">
+        <img src="{{ asset('admin_assets/img/logo/gawang-gamat.png') }}" alt="Gawang Gamat" class="h-12 w-auto object-contain">
     </div>
 </div>
